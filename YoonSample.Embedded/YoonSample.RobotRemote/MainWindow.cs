96d29044163a62c187480bd68199adb225e2fa6f
Maroomir Yoon <maroomir@gmail.com>
2021-09-07 16:18:21 +0900

    Add the RobotRemote sample

---
+++ YoonSample.Embedded/YoonSample.RobotRemote/MainWindow.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Gtk;
+using YoonSample.RobotRemote;
+using YoonFactory.Param;
+using YoonFactory.Log;
+using YoonFactory.Comm;
+using YoonFactory.Robot.UniversialRobot;
+
+public partial class MainWindow : Gtk.Window
+{
+    private YoonUR pRemote = null;
+    private YoonConsoler pCLM = null;
+    private YoonParameter pParamManager = null;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        InitComponent();
+        InitEvent();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    private void InitComponent()
+    {
+        pCLM = new YoonConsoler(30);
+        pCLM.Write("Init Robots");
+        pRemote = new YoonUR();
+        pRemote.OnShowMessageEvent += OnShowMessageEvent;
+        pRemote.OnShowReceiveDataEvent += OnShowReceiveDataEvent;
+        pCLM.Write("Init Parameter");
+        ParameterClass pParam = new ParameterClass();
+        pParamManager = new YoonParameter(pParam, typeof(ParameterClass));
+        pParamManager.LoadParameter(true);
+    }
+
+    private void OnShowMessageEvent(object sender, MessageArgs e)
+    {
+        pCLM.Write("[COMM] " + e.Message);
+    }
+
+    private void OnShowReceiveDataEvent(object sender, BufferArgs e)
+    {
+        pCLM.Write("[RECV] " + e.StringData);
+    }
+
+    private void InitEvent()
+    {
+        pCLM.Write("Init Events");
+        button_Power.Clicked += OnPowerButtonClick;
+        button_SendOn.Clicked += OnSendOnButtonClick;
+        button_SendOff.Clicked += OnSendOffButtonClick;
+    }
+
+    private void OnPowerButtonClick(object sender, EventArgs e)
+    {
+        pRemote.PowerOnRobot();
+        if (pRemote.PowerOffRobot())
+            pCLM.Write("[SEND] Power Off");
+    }
+
+    private void OnSendOnButtonClick(object sender, EventArgs e)
+    {
+        ParameterClass pParam = pParamManager.Parameter as ParameterClass;
+        if (pRemote.SendSocket(pParam.OnMessage))
+            pCLM.Write("[SEND] " + pParam.OnMessage);
+    }
+
+    private void OnSendOffButtonClick(object sender, EventArgs e)
+    {
+        ParameterClass pParam = pParamManager.Parameter as ParameterClass;
+        if (pRemote.SendSocket(pParam.OffMessage))
+            pCLM.Write("[SEND] " + pParam.OffMessage);
+    }
+}
---
+++ YoonSample.Embedded/YoonSample.RobotRemote/ParameterClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using YoonFactory;
+namespace YoonSample.RobotRemote
+{
+    public class ParameterClass : IYoonParameter
+    {
+        public string OnMessage { get; set; } = "1";
+        public string OffMessage { get; set; } = "2";
+
+        public IYoonParameter Clone()
+        {
+            ParameterClass pParam = new ParameterClass();
+            pParam.OnMessage = OnMessage;
+            pParam.OffMessage = OffMessage;
+            return pParam;
+        }
+
+        public void CopyFrom(IYoonParameter pParam)
+        {
+            if(pParam is ParameterClass pParamPacket)
+            {
+                pParamPacket.OnMessage = OnMessage;
+                pParamPacket.OffMessage = OffMessage;
+            }
+        }
+
+        public bool Equals(IYoonParameter pParam)
+        {
+            if(pParam is ParameterClass pParamPacket)
+            {
+                return pParamPacket.OnMessage == OnMessage &&
+                    pParamPacket.OffMessage == OffMessage;
+            }
+            return false;
+        }
+
+        public int GetLength()
+        {
+            return typeof(ParameterClass).GetProperties().Length;
+        }
+
+        public void Set(params string[] pArgs)
+        {
+            if (pArgs.Length != GetLength()) return;
+            OnMessage = pArgs[0];
+            OffMessage = pArgs[1];
+        }
+    }
+}
